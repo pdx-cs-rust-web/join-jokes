b3df1c75135a9b40af480c75931a46d1216bc025
Bart Massey <bart.massey@gmail.com>
2025-04-24 17:21:20 -0700

    added remaining joke fields

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,25 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize)]
 struct Joke {
+    id: String,
     whos_there: String,
     answer_who: String,
+    tags: Vec<String>,
+    source: String,
 }
 
 fn main() {
     let mut jokes = Vec::new();
     for path in fs::read_dir("jokes").unwrap() {
         let path = path.unwrap().path();
-        let file = fs::File::open(path).unwrap();
-        let joke: Joke = serde_json::from_reader(file).unwrap();
+        let file = fs::File::open(&path).unwrap();
+        let joke: Joke = match serde_json::from_reader(file) {
+            Ok(joke) => joke,
+            Err(e) => {
+                eprintln!("{}: {}", path.display(), e);
+                continue;
+            }
+        };
         jokes.push(joke);
     }
     let out_file = fs::File::create("jokes.json").unwrap();
